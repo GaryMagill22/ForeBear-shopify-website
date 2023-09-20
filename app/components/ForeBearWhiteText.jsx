36d01fed6461caeecf52330db3b05a5a34750ddc
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-09-19 18:46:01 -0600

    Working Hydrogen with 2 components implemented

---
+++ app/components/ForeBearWhiteText.jsx
@@ -0,0 +1,11 @@
+
+
+
+export function ForeBearText() {
+    return (
+        <div >
+            <img id="forebearWhiteText" src="WhiteForeBearTextOnly.png" alt="White ForeBear" />
+        </div>
+    )
+}
+
