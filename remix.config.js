822bad9b65d64d6de3ed34faef65eb56e7d59f27
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-09-29 11:40:04 -0600

    commit

--- remix.config.js
+++ remix.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain: 'forebearwebsite.myshopify.com',
+    storeDomain: 'fore-2-tour.myshopify.com',
     storefrontToken: '2d7f53f52160378c4b19e725846904d8',
     storefrontApiVersion: '2023-07',
   },
@@ -47,4 +47,3 @@ export default defineConfig({
     maxAge: 60 * 60 * 24 * 365,
   }),
 });
-
